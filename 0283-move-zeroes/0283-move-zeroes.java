b6623132bdea1a663809de488f862453d8b9abe5
Monalisa Sahoo <157225761+Lisa2801@users.noreply.github.com>
2024-08-10 00:43:07 +0530

    Time: 2 ms (82.86%), Space: 45.8 MB (76.91%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,14 +1,25 @@
 class Solution {
     public void moveZeroes(int[] nums) {
+        //method 1 Time=O(n) Space=O(1)
+        // int index=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]!=0){
+        //         nums[index++]=nums[i];
+        //     }
+        // }
+        // while(index<nums.length){
+        //     nums[index++]=0;
+        // }
+
+        //method 2 Time=O(n) Space=O(1)
         int index=0;
         for(int i=0;i<nums.length;i++){
             if(nums[i]!=0){
-                nums[index++]=nums[i];
+                int temp=nums[i];
+                nums[i]=nums[index];
+                nums[index]=temp;
+                index++;
             }
         }
-        while(index<nums.length){
-            nums[index++]=0;
-        }
-        
     }
 }
