bdc4d01976df6191d47c10abef8f42920f0bbff4
Monalisa Sahoo <157225761+Lisa2801@users.noreply.github.com>
2024-08-16 16:42:12 +0530

    Time: 3 ms (85.55%), Space: 40.3 MB (47.64%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -1,25 +1,5 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
-        // if(c==0||c==1) return true;
-        // int start=1;
-        // int end=c/2;
-        // int temp=0;
-        // while(start<=end){
-        //     int mid=start+(end-start)/2;
-        //     if(mid*mid==c-1||mid*mid==c) return true;
-        //     else if(mid>c/mid){
-        //         end=mid-1;
-        //     }
-        //     else{
-        //         start=mid+1;
-        //     }
-        // }
-        // temp=end;
-        // for(int i=1;i<=temp;i++){
-        //     if(i*i+temp*temp==c) return true;
-        //     else return false;
-        // }
-        // return false;
         if(c==0||c==1) return true;
         long start=0;
         long end=(int)(Math.sqrt(c));
